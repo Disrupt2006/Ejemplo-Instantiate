ce7a8bd45a31197e38327eeb4f4ab262aa95e087
Thiago Orué <89083464+Disrupt2006@users.noreply.github.com>
2022-05-19 09:40:36 -0300

    Initial commit

---
+++ Assets/cubeclone.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeclone : MonoBehaviour
+{
+
+    public GameObject prefab;
+    public GameObject clon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i<3; i++)
+        {
+            // insancia este objeto (duplica)
+            clon = Instantiate(prefab);
+            clon.transform.Translate(i, 0, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
